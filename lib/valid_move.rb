bb11b237510883d23be61d83eacb21a67e6422a6
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-30 13:19:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,7 +6,8 @@
 #Not already filled with a token.
 
 def valid_move?(board,index_num)
-
+if 
+end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
